e19da9c40d10e53c20e0575d976cb9760bce277b
Yuragranovsky <yuragranovsky@gmail.com>
2026-02-17 18:39:02 +0200

    one time date updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
-
+import { useEffect, useState } from "react"
 import Timer from "./components/Timer"
 
 
 function App() {
- 
+  const [date, setDate] = useState<Date> (new Date())
+  useEffect(()=>{
+    function tic(){
+      setDate(new Date())
+    }
+    const intervalId = setInterval(tic, 1000)
+    return () => clearInterval(intervalId)
+  }, [])
 
   return <>
-      <Timer city="Tel-Aviv" timeZone="Asia/Jerusalem" dateTime={new Date()}/>
+      <Timer city="Tel-Aviv" timeZone="Asia/Jerusalem" dateTime={date}/>
   </>
 }
 
